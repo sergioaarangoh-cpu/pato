456cd66756173aaf12cdd604f82e162de02c55e3
juancho-2006 <juans.echeverrif@autonoma.edu.co>
2026-05-13 14:58:35 -0500

    Agrega javadoc a GameController

--- OldPato/src/main/java/controller/GameController.java
+++ OldPato/src/main/java/controller/GameController.java
@@ -1,9 +1,15 @@
 package main.java.controller;
 
+/**
+ * Controla el estado de toda la partida
+ */
 public class GameController implements Runnable {
     private volatile int remainingSeconds = 120;
     private boolean running = true;
 
+    /**
+     * Ejecuta la cuenta regresiva del juego mientras haya tiempo disponible.
+     */
     @Override
     public void run() {
         while (running && remainingSeconds > 0) {
@@ -20,6 +26,11 @@ public class GameController implements Runnable {
         }
     }
 
+    /**
+     * Obtiene los segundos restantes de la partida.
+     *
+     * @return cantidad de segundos disponibles antes de que termine el tiempo.
+     */
     public int getRemainingSeconds() {
         return remainingSeconds;
     }
